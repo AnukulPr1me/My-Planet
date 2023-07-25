419817be6ae6a1a20171565b7126717d01caf085
Anukul <101039186+AnukulPr1me@users.noreply.github.com>
2023-07-25 17:11:43 +0530

    rgb color changes

--- src/main.js
+++ src/main.js
@@ -67,8 +67,31 @@ const loop = () => {
 }
 loop()
 
-
+//timeline magic
 const t1 = gsap.timeline({defaults: {duration: 1} })
 t1.fromTo(mesh.scale, {z:0, x:0, y:0 }, {z:1, x:1, y:1} )
 t1.fromTo("nav", {y: "-100%" }, {y: "0%"})
 t1.fromTo(".title", {opacity: 0}, {opacity: 1})
+
+
+//mouse trail color
+let mouseDown = false
+let rgb = [];
+window.addEventListener("mousedown", ()=> (mouseDown = true))
+window.addEventListener("mouseup", ()=> (mouseDown = false))
+
+window.addEventListener('mousemove', (e) =>{
+    if (mouseDown){
+        rgb = [
+            Math.round((e.pageX / sizes.width) * 255),
+            Math.round((e.pageY / sizes.height) * 255), 150,
+        ]
+        //animate
+        let newColor = new THREE.Color(`rgb(${rgb.join(",")})`)
+        gsap.to(mesh.material.color, {
+            r: newColor.r,
+            g: newColor.g,
+            b: newColor.b,
+        })
+    }
+});
