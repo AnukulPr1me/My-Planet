037a4050367989a074c862653667524dc47e8374
Anukul <101039186+AnukulPr1me@users.noreply.github.com>
2023-07-25 21:37:37 +0530

    added stars

--- src/main.js
+++ src/main.js
@@ -27,6 +27,7 @@ const sizes = {
 //light
 const light = new THREE.PointLight(0xffffff, 1, 100)
 light.position.set(10, 10, 10);
+light.intensity = 1.25
 scene.add(light)
 
 //camera
@@ -36,7 +37,7 @@ scene.add(camera)
 
 //render
 const canvas = document.querySelector(".webgl")
-const renderer = new THREE.WebGLRenderer({canvas})
+const renderer = new THREE.WebGLRenderer({canvas, antialias: true})
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(2);
 renderer.render(scene, camera)
@@ -95,3 +96,65 @@ window.addEventListener('mousemove', (e) =>{
         })
     }
 });
+
+// Rest of your existing code...
+
+// Function to generate random positions and sizes for stars and avoid positions too close to the sphere
+function generateStars(numStars, spherePosition, minDistance) {
+    const stars = [];
+    for (let i = 0; i < numStars; i++) {
+      let x, y, z;
+      do {
+        x = (Math.random() - 0.5) * 100;
+        y = (Math.random() - 0.5) * 100;
+        z = (Math.random() - 0.5) * 100;
+      } while (spherePosition.distanceTo(new THREE.Vector3(x, y, z)) < minDistance);
+  
+      const size = Math.random() * 0.05 + 0.01; // Random size between 0.01 and 0.06
+      const color = 0xffffff; // White color
+  
+      stars.push({ x, y, z, size, color });
+    }
+    return stars;
+  }
+  
+  // Sphere position and minimum distance from the sphere for star generation
+  const spherePosition = new THREE.Vector3(0, 0, 0); // Replace with the actual sphere position
+  const minDistanceFromSphere = 2; // Minimum distance from the sphere
+  
+  // Generate stars with random sizes
+  const numStars = 1000;
+  const stars = generateStars(numStars, spherePosition, minDistanceFromSphere);
+  
+  // Create star meshes
+  const starGeometry = new THREE.SphereGeometry();
+  const starMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
+  
+  for (const star of stars) {
+    const starMesh = new THREE.Mesh(starGeometry, starMaterial.clone());
+    starMesh.position.set(star.x, star.y, star.z);
+    starMesh.scale.set(star.size, star.size, star.size);
+    scene.add(starMesh);
+  }
+  
+  // Random blinking effect using GSAP
+  function blinkStars(starMeshes) {
+    for (const starMesh of starMeshes) {
+      gsap.to(starMesh.material, {
+        opacity: 0.2,
+        duration: Math.random() * 0.5 + 0.2,
+        repeat: -1,
+        yoyo: true,
+        delay: Math.random() * 3, // Random delay for each star
+      });
+    }
+  }
+  
+  // Call the blinkStars function to start the blinking effect
+  blinkStars(scene.children.filter((child) => child instanceof THREE.Mesh));
+  
+  // Rest of your existing code...
+  
+  
+  
+  
